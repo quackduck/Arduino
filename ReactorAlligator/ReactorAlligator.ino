9f76b23bb20ee92b620b131dad9e653a74287c9b
Vinithr-python <54896116+Vinithr-python@users.noreply.github.com>
2020-05-28 15:52:57 +0400

    Update ReactorAlligator.ino

--- ReactorAlligator/ReactorAlligator.ino
+++ ReactorAlligator/ReactorAlligator.ino
@@ -1,9 +1,11 @@
+int led = 3;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(led, HIGH);
 }
